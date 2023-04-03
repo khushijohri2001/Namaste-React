94a3027ce140d4de992d366784fb1acc2bb9ad07
Khushi Johri <khushijohri2001@gmail.com>
2023-04-03 18:07:47 +0530

    Episode-2 parcel and react modules added

---
+++ Episode-2/app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+const parent = React.createElement("div", { id: "parent" }, [React.createElement(
+    "div",
+
+    { id: "child" },
+    [
+      React.createElement("h1", {}, "I'm an H1 Tag"),
+      React.createElement("h2", {}, "I'm an H2 Tag"),
+    ]
+  ),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an H1 Tag"),
+    React.createElement("h2", {}, "I'm an H2 Tag"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World in React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
